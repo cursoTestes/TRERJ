c9c37eabd2f03d712d8960b9eace616ae6a4a46c
cfc <cfc@ufrj.br>
2013-01-31 14:01:04 -0200

    Import Inicial

---
+++ SistemaVendas/testes/br/tre/TestCalculadora.java
@@ -0,0 +1,18 @@
+package br.tre;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class TestCalculadora {
+
+	@Test
+	public void test_Nome_Teste() {
+
+		
+		Assert.assertEquals(1, 1);
+		assertEquals(1, 1);
+	}
+	
+}
